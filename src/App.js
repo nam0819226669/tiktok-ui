e1446b87a231348af1bff9e835bd3555ab6c416a
nam0819226669 <76526213+nam0819226669@users.noreply.github.com>
2023-11-28 10:37:42 +0700

    bai 60

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import logo from './logo.svg';
-import './compunents/Button'
+import './compunents/Button';
 import './App.css';
 import Button from './compunents/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <Button></Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button></Button>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
