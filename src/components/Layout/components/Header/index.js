be6487c46b083c9f5ecfa40bb75247b6aa526c3b
nam0819226669 <76526213+nam0819226669@users.noreply.github.com>
2023-11-28 18:07:36 +0700

    64 làm phut thứ 10

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,16 @@
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
-
+import images from 'assets/images';
 const cx = classNames.bind(style);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo.default} alt="Tiktok"></img>
+                </div>
+            </div>
         </header>
     );
 }
