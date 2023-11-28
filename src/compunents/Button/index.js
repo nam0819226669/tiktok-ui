a99a798e6938fbe08d86458f72dfd65227afbd3f
nam0819226669 <76526213+nam0819226669@users.noreply.github.com>
2023-11-28 10:20:04 +0700

    Bai 59

---
+++ src/compunents/Button/index.js
@@ -0,0 +1,6 @@
+import myAlert from "../myAlert.js"
+
+function Button(){
+    return <button onClick={myAlert}>Click me!</button>
+}
+export default Button
---
+++ src/compunents/myAlert.js
@@ -0,0 +1,4 @@
+const myAlert = () => {
+    alert("My alert")
+}
+export default myAlert
